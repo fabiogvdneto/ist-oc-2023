ecb4746380706b5017b302b6804d81c8dfc261b5
Fabio Neto <fabiogvdneto@gmail.com>
2023-10-05 14:03:52 +0100

    Removed some nonsense comments.

--- 4.1/L1Cache.c
+++ 4.1/L1Cache.c
@@ -44,6 +44,7 @@ void initCache() { L1Cache.init = 0; }
 void accessL1(uint32_t address, uint8_t *data, uint32_t mode) {
   CacheLine* lines = L1Cache.lines;
 
+  // Make sure cache is already initialized. If not, initialize first.
   if (!L1Cache.init) {
     for (int i = 0; i < sizeof(lines); i++) {
       lines[i].valid = 0;
@@ -66,29 +67,30 @@ void accessL1(uint32_t address, uint8_t *data, uint32_t mode) {
 
   lines += index;
 
+  // Make sure data block is present in cache. If not, fetch block from RAM.
   if (!lines->valid || lines->tag != tag) {
     uint32_t memAddress = (address >> bits_offset) << bits_offset;
     uint8_t tempBlock[BLOCK_SIZE];
 
-    accessDRAM(memAddress, tempBlock, MODE_READ); // get new block from DRAM
+    accessDRAM(memAddress, tempBlock, MODE_READ);
 
-    if ((lines->valid) && (lines->dirty)) { // line has dirty block
-      accessDRAM(memAddress, lines, MODE_WRITE); // then write back old block
+    if ((lines->valid) && (lines->dirty)) {
+      accessDRAM(memAddress, lines, MODE_WRITE);
     }
 
-    memcpy(lines, tempBlock, BLOCK_SIZE); // copy new block to cache line
+    memcpy(lines, tempBlock, BLOCK_SIZE);
     lines->valid = 1;
     lines->dirty = 0;
     lines->tag = tag;
-  } // if miss, then replaced with the correct block
+  }
 
-  if (mode == MODE_READ) {    // read data from cache line
+  if (mode == MODE_READ) {
     memcpy(data, lines->data[offset], WORD_SIZE);
     time += L1_READ_TIME;
     return;
   }
 
-  if (mode == MODE_WRITE) { // write data from cache line
+  if (mode == MODE_WRITE) {
     memcpy(lines->data[offset], data, WORD_SIZE);
     time += L1_WRITE_TIME;
     lines->dirty = 1;
