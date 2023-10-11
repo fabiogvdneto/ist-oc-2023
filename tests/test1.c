b41d780031fa213fe5323f8f18a16045be6f5c79
Fabio Neto <fabiogvdneto@gmail.com>
2023-10-11 18:40:53 +0100

    Fixed 4.1 tests.

--- tests/test1.c
+++ tests/test1.c
@@ -15,18 +15,17 @@ int main() {
   uint32_t val3 = 33;
   uint32_t val4 = 44;
 
-  uint8_t* pvalue = (uint8_t*) &value;
+  uint32_t addr1 = 0x104;
+  uint32_t addr2 = 0x108;
+  uint32_t addr3 = 0x10C;
+  uint32_t addr4;
 
+  uint8_t* pvalue = (uint8_t*) &value;
   uint8_t* pval1 = (uint8_t*) &val1;
   uint8_t* pval2 = (uint8_t*) &val2;
   uint8_t* pval3 = (uint8_t*) &val3;
   uint8_t* pval4 = (uint8_t*) &val4;
 
-  uint32_t addr1 = 0x4;
-  uint32_t addr2 = 0x8;
-  uint32_t addr3 = 0xC;
-  uint32_t addr4;
-
   write(addr1, pval1);
   printf("write\t%d\t%u\t%d\n", addr1, val1, getTime());
   write(addr2, pval2);
@@ -36,16 +35,16 @@ int main() {
 
   read(addr1, pvalue);
   printf("read\t%d\t%u\t%d\n", addr1, value, getTime());
-  read(addr1, pvalue);
+  read(addr2, pvalue);
   printf("read\t%d\t%u\t%d\n", addr2, value, getTime());
-  read(addr1, pvalue);
+  read(addr3, pvalue);
   printf("read\t%d\t%u\t%d\n", addr3, value, getTime());
 
   // SECOND ROUND: same as first round, but different index this time.
 
-  addr1 = 0x104;
-  addr2 = 0x108;
-  addr3 = 0x10C;
+  addr1 = 0x204;
+  addr2 = 0x208;
+  addr3 = 0x20C;
 
   write(addr1, pval1);
   printf("write\t%d\t%u\t%d\n", addr1, val1, getTime());
@@ -56,9 +55,9 @@ int main() {
 
   read(addr1, pvalue);
   printf("read\t%d\t%u\t%d\n", addr1, value, getTime());
-  read(addr1, pvalue);
+  read(addr2, pvalue);
   printf("read\t%d\t%u\t%d\n", addr2, value, getTime());
-  read(addr1, pvalue);
+  read(addr3, pvalue);
   printf("read\t%d\t%u\t%d\n", addr3, value, getTime());
 
   // THIRD ROUND: conflicts (same index, different tag).
@@ -86,6 +85,5 @@ int main() {
   read(addr1, pvalue);
   printf("read\t%d\t%u\t%d\n", addr4, value, getTime());
 
-  
   return 0;
 }
